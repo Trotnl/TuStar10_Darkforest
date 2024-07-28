f71dc033fca942c63da8aff73d5caaa10adf5a44
Yi-Sun-superyoghurt <124099235+Yi-Sun-superyoghurt@users.noreply.github.com>
2024-07-28 11:13:42 -0400

    start_scene_startbutton

---
+++ Assets/ArtMaterials/Start_Game_UI/Touch Button.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CustomButton : MonoBehaviour, IPointerDownHandler
+{
+    public string nextSceneName; // 目标场景名称
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Cube 被点击了！");
+        // 这里可以添加你想要执行的操作
+        SwitchToNextScene();
+    }
+
+    private void SwitchToNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+}
